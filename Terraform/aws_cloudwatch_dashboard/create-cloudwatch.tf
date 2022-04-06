f2cd6e501f8e8f621adb9439829e246f309aa04c
Nabeel Hassan <thenabeelhassan@gmail.com>
2022-04-07 01:11:55 +0500

    included NetworkIn,NetworkOut,DiskReadBytes,DiskWriteBytes metrics

--- Terraform/aws_cloudwatch_dashboard/create-cloudwatch.tf
+++ Terraform/aws_cloudwatch_dashboard/create-cloudwatch.tf
@@ -37,29 +37,90 @@ resource "aws_cloudwatch_dashboard" "main" {
       }
     },
     {
-      "type": "text",
+      "type": "metric",
       "x": 0,
-      "y": 7,
-      "width": 3,
-      "height": 3,
+      "y": 0,
+      "width": 12,
+      "height": 6,
       "properties": {
-        "markdown": "Some text"
+        "metrics": [
+          [
+            "AWS/EC2",
+            "NetworkIn",
+            "InstanceId",
+            "${var.ec2-instance}"
+          ]
+        ],
+        "period": 300,
+        "stat": "Average",
+        "region": "us-east-1",
+        "title": "${var.ec2-instance} - Network In"
+      }
+    },
+    {
+      "type": "metric",
+      "x": 0,
+      "y": 0,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "AWS/EC2",
+            "NetworkOut",
+            "InstanceId",
+            "${var.ec2-instance}"
+          ]
+        ],
+        "period": 300,
+        "stat": "Average",
+        "region": "us-east-1",
+        "title": "${var.ec2-instance} - Network Out"
+      }
+    },
+    {
+      "type": "metric",
+      "x": 0,
+      "y": 0,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "AWS/EC2",
+            "DiskReadBytes",
+            "InstanceId",
+            "${var.ec2-instance}"
+          ]
+        ],
+        "period": 300,
+        "stat": "Average",
+        "region": "us-east-1",
+        "title": "${var.ec2-instance} - Disk Read Bytes"
+      }
+    },
+    {
+      "type": "metric",
+      "x": 0,
+      "y": 0,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "AWS/EC2",
+            "DiskWriteBytes",
+            "InstanceId",
+            "${var.ec2-instance}"
+          ]
+        ],
+        "period": 300,
+        "stat": "Average",
+        "region": "us-east-1",
+        "title": "${var.ec2-instance} - Disk Write Bytes"
       }
     }
   ]
 }
   EOF
-}
-
-resource "aws_cloudwatch_metric_alarm" "ec2-cpu-HighUtilization" {
-  alarm_name = "${var.ec2-instance} - High CPU Utilization"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods = "2"
-  metric_name = "CPUUtilization"
-  namespace = "AWS/EC2"
-  period = "120"
-  statistic = "Average"
-  threshold = "80"
-  alarm_description = "This alaram trigers at CPU Utlization reaches or exceed 80%"
-  insufficient_data_actions = []
 }
